bf7d580555dc8d4a09a78d5a68c2565ecd0d4f3e
Atuoha <atuohainitiatives@gmail.com>
2022-10-28 05:50:23 +0100

    Task: Google authentication

--- lib/screens/authentication/signup_acknowledge.dart
+++ lib/screens/authentication/signup_acknowledge.dart
@@ -1,16 +1,21 @@
-import 'package:diaree/resources/route_manager.dart';
-import 'package:diaree/screens/authentication/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import "package:flutter/material.dart";
 import '../../resources/assets_manager.dart';
-import '../../resources/font_manager.dart';
 import '../../resources/styles_manager.dart';
 import '../../resources/values_manager.dart';
 
 class AuthAcknowledgementScreen extends StatelessWidget {
-  const AuthAcknowledgementScreen({super.key});
+  const AuthAcknowledgementScreen({super.key, required this.credential});
+
+  final UserCredential credential;
 
   @override
   Widget build(BuildContext context) {
+    // navigate to main screen
+    void navigateToMainScreen() {
+      Navigator.of(context).pushReplacementNamed("");
+    }
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 30),
@@ -20,33 +25,29 @@ class AuthAcknowledgementScreen extends StatelessWidget {
             children: [
               Image.asset(AssetManager.success),
               const SizedBox(height: AppSize.s35),
-
               Text(
                 'Great! You\'ve \nsuccessfully created an account.',
                 textAlign: TextAlign.center,
                 style: getHeadingStyle2(),
               ),
-
             ],
           ),
         ),
       ),
-      bottomSheet:  Container(
-        height:60,
-        padding: const EdgeInsets.only(bottom:10),
+      bottomSheet: Container(
+        height: 60,
+        padding: const EdgeInsets.only(bottom: 10),
         alignment: Alignment.bottomCenter,
-        child:
-        ElevatedButton(
+        child: ElevatedButton(
           style: ElevatedButton.styleFrom(
             padding: const EdgeInsets.symmetric(
               horizontal: 60,
               vertical: 8,
             ),
           ),
-          onPressed: () => Navigator.of(context).pushNamed(""),
+          onPressed: () => navigateToMainScreen(),
           child: const Text('Jump in'),
         ),
-
       ),
     );
   }
